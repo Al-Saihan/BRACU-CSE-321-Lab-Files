f42f1105d608c3e39f2472ba392c322e2f527d0b
Al-Saihan <al.saihan.bafsd.5@gmail.com>
2025-03-29 01:47:23 +0600

    Added Task 1 of Assignment1

---
+++ Assignment_1/task1/task1.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    char buffer[100] = "Random Text";
+
+    int file = open(strcat(argv[1], ".txt"), O_CREAT | O_WRONLY | O_TRUNC, 0666); 
+
+    while (1){        
+        if (strcmp("-1\n", buffer) == 0){
+            printf("\n---Exiting Program---\n\n");
+            break;
+        }
+
+        memset(buffer, '\0', sizeof(buffer));
+        read(0, buffer, 100);
+        write(file, buffer, strlen(buffer));
+    }
+}
