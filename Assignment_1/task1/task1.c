09279c02874f70dc0ef50a8244265f23d4a5bc43
Al-Saihan <al.saihan.bafsd.5@gmail.com>
2025-04-08 20:15:32 +0600

    Added Initial Sentences At The Start

--- Assignment_1/task1/task1.c
+++ Assignment_1/task1/task1.c
@@ -9,6 +9,8 @@ int main(int argc, char *argv[])
 
     int file = open(strcat(argv[1], ".txt"), O_CREAT | O_RDWR | O_TRUNC, 0666);
 
+    printf("\nA new file has been opened, with the name: %s\n\n", argv[1]);
+    printf("Start writing anything, to write in to the file!\n\nWrite '-1' to stop the writing process!\n\n");
     while (1)
     {
         if (strcmp("-1\n", buffer) == 0)
