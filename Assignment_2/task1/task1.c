a966d49729fe0a8beb3c74a510181ee5af140a29
Al-Saihan <al.saihan.bafsd.5@gmail.com>
2025-04-15 23:53:42 +0600

    Added Task 1 of Assignment 2

--- Assignment_2/task1/task1.c
+++ Assignment_2/task1/task1.c
@@ -5,34 +5,64 @@
 
 int a = 0;
 int b = 1;
+int total_numbers, total_searches;
 
-void *fibonacci_sequence(void *terms, void *arr)
+void *fibonacci_sequence(void *nums)
 {
-    int *N = terms;
+    int *array = (int *)nums;
 
-    return NULL;
+    array[0] = a;
+    array[1] = b;
+    printf("a[0] = %d\n", array[0]);
+    printf("a[1] = %d\n", array[1]);
+
+    for (int i = 2; i < total_numbers; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+        printf("a[%d] = %d\n", i, array[i]);
+    }
 }
 
-void *fibonacci_search(void *searches)
+void *fibonacci_search(void *nums)
 {
-    return NULL;
+    int *array = (int *)nums;
+
+    int s;
+
+    for (int i = 1; i < total_searches; i++)
+    {
+        printf("Enter Search %d: ", i);
+        scanf("%d", &s);
+        printf("Result of search #%d = ", i);
+        if ((s >= total_numbers) || (s < 0))
+        {
+            printf("-1\n");
+        }
+        else
+        {
+            printf("%d\n", array[s]);
+        }
+    }
 }
 
 int main(void)
 {
     pthread_t thread1, thread2;
-    int total_terms, total_searches;
-    void *numbers = malloc(sizeof(int) * 8);
 
     printf("Enter the term of fibonacci sequence: \n");
-    scanf("%d\n", total_terms);
+    scanf("%d", &total_numbers); //
 
     printf("How many numbers you are willing to search?: \n");
-    scanf("%d\n", total_searches);
+    scanf("%d", &total_searches);
 
-    pthread_create(&thread1, NULL, fibonacci_sequence, (void *)total_terms, *numbers);
+    total_numbers++;
+    total_searches++;
+
+    int *numbers = malloc(total_numbers * sizeof(int));
+
+    pthread_create(&thread1, NULL, fibonacci_sequence, (void *)numbers);
     pthread_join(thread1, NULL);
 
-    pthread_create(&thread2, NULL, fibonacci_search, NULL);
+    pthread_create(&thread2, NULL, fibonacci_search, (void *)numbers);
     pthread_join(thread2, NULL);
 }
